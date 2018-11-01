791c026c0e78e03823c4c076d9f0203f2a59182c
Aaron Ali <aaron@causal.ca>
2018-11-01 11:07:19 -0400

    return

--- tonedef/main.js
+++ tonedef/main.js
@@ -80,7 +80,7 @@ module.exports.loop = function () {
 		if(spawn.room.energyAvailable > 650) {
 			// respawn seeker
 			var seeker = _.filter(Game.creeps, (creep) => creep.memory.role == 'seeker');
-			if(seeker.length < 3) {
+			if(seeker.length < 2) {
 				var newName = 'seek' + Game.time;
 				console.log('Spawning new seeker: ' + newName);
 				spawn.spawnCreep([CLAIM,MOVE], newName, 
