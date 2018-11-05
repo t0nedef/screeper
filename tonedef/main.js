b1d9c908d3388c60752f75c1f23e50708c1321c7
Aaron Ali <aaron@causal.ca>
2018-11-05 12:37:48 -0500

    tweak

--- tonedef/main.js
+++ tonedef/main.js
@@ -58,7 +58,7 @@ module.exports.loop = function () {
 		if(spawn.room.energyAvailable > 200) {
 			// respawn builder
 			var geners = _.filter(creeps, (creep) => creep.memory.role == 'general');
-			if(geners.length < 4) {
+			if(geners.length < 5) {
 				var newName = 'gener' + Game.time;
 				console.log('Spawning new gener: ' + newName);
 				spawn.spawnCreep([WORK,CARRY,MOVE], newName, 
