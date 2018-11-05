3364a05d8fdde03fa4569288aff99ec9b110adc2
Aaron Ali <aaron@causal.ca>
2018-11-04 23:09:28 -0500

    tweak

--- tonedef/role.harvester.js
+++ tonedef/role.harvester.js
@@ -33,6 +33,13 @@ var roleHarvester = {
 						if(creep.transfer(struct, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 							creep.moveTo(struct);
 						}
+					} else {
+						var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+						if(target) {
+							if(creep.build(target) == ERR_NOT_IN_RANGE) {
+								creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+							}
+						}
 					}
 				}
 			}
