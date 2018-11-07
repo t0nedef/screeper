0ea7e073f795975bc26ed4c22ad408b47363b604
Aaron Ali <aaron@causal.ca>
2018-11-07 09:36:48 -0500

    tweak

--- tonedef/role.upgrader.js
+++ tonedef/role.upgrader.js
@@ -5,16 +5,27 @@ var roleUpgrader = {
 	run: function(creep) {
 		if(creep.carry.energy < creep.memory.targetEnergy) {
 			creep.memory.targetEnergy = creep.carryCapacity;
+			// look for energy hanging around
+			// look for minerals hanging around
 			var source = creep.room.storage;
 			if(source) {
 				if(creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
 				}
 			} else {
-				var sources = creep.room.find(FIND_SOURCES);
-				source = sources[creep.memory.source];
-				if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+				var structs = _.filter(creep.room.find(FIND_STRUCTURES),
+					(structure) => structure.structureType == STRUCTURE_CONTAINER && _.sum(structure.store) > 0);
+				if(structs.length > 0) {
+					var struct = creep.pos.findClosestByRange(structs);
+					if(creep.withdraw(struct, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+						creep.moveTo(struct, {visualizePathStyle: {stroke: '#ffaa00'}});
+					}
+				} else {
+					var sources = creep.room.find(FIND_SOURCES);
+					var source = sources[creep.memory.source];
+					if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+						creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+					}
 				}
 			}
 		} else {
